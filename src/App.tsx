806fb5ddfbea996ebcdf7fa82db74bb465408a18
MichaelOsr <michaeloctaviansr@gmail.com>
2026-02-22 18:39:32 +0700

    Add skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import About from "./pages/About"
 import Hero from "./pages/Hero"
 import Navbar from "./pages/NavBar"
+import Skills from "./pages/Skills"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Skills />
     </div>
   )
 }
